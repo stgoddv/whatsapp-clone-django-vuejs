b563f56337215e49c1dae2e4e1ed2a30cd9e4354
santiago <stgoddv@gmail.com>
2020-01-03 14:48:37 -0300

    added support to http requests to backend from frontend

--- djchat/frontend/src/views/Home.vue
+++ djchat/frontend/src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <img
+      alt="Vue logo"
+      src="../assets/logo.png"
+    />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
@@ -8,11 +11,23 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "@/backend";
 
 export default {
   name: "home",
   components: {
     HelloWorld
+  },
+  created() {
+    console.log("llamando a axios");
+    axios
+      .get("/api/v1/user/")
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 };
 </script>
