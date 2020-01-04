5b681c1402c5c623a77368f710ad767aacb67981
santiago <stgoddv@gmail.com>
2020-01-04 03:05:34 -0300

    added endpoint for listing registerd users

--- djchat/users/api/views.py
+++ djchat/users/api/views.py
@@ -2,6 +2,17 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from users.api.serializers import UserSerializer
 
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class UsersAPIView(APIView):
+
+    def get(self, request):
+        serializer = UserSerializer(User.objects.all(), many=True)
+        return Response(serializer.data)
+
 
 class CurrentUserAPIView(APIView):
 
