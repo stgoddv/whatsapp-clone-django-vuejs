91df8812a1f6a0b84ce4eb29fed67c4ed0c35f68
santiago <stgoddv@gmail.com>
2020-01-05 15:57:21 -0300

    removed some outdated docs

--- djchat/frontend/src/store/store.js
+++ djchat/frontend/src/store/store.js
@@ -1,11 +1,3 @@
-/*=========================================================================================
-  File Name: store.js
-  Description: Vuex store
-  ----------------------------------------------------------------------------------------
-  Item Name: Vuexy - Vuejs, HTML & Laravel Admin Dashboard Template
-  Author: Pixinvent
-  Author URL: http://www.themeforest.net/user/pixinvent
-==========================================================================================*/
 
 import Vue from "vue";
 import Vuex from "vuex";
