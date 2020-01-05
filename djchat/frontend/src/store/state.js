959cc5eed6362e8bf7c4bd3f7e663bdab8f787ff
santiago <stgoddv@gmail.com>
2020-01-05 16:11:13 -0300

    added vuex state structure

--- djchat/frontend/src/store/state.js
+++ djchat/frontend/src/store/state.js
@@ -1,5 +1,38 @@
 
 const state = {
+  // The room that's currently selected
+  selectedRoom: null,
+
+  /*
+
+  rooms: [
+    id,
+    kind,
+    name,
+    participants: [
+      ...ids
+    ],
+    messages: [
+      ...ids
+    ],
+    fetchRecents
+  ]
+
+  messages: [
+    id,
+    title,
+    body,
+    timestamp,
+    isOwner,
+    room
+  ]
+
+  users: [
+    id,
+    username
+  ]
+
+  */
 
 };
 
