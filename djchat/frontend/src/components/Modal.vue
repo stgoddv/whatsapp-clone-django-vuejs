c05a4f08e72eb27d24e038b9377bf4616906320b
santiago <stgoddv@gmail.com>
2020-01-06 03:37:00 -0300

    added custom modal component

--- djchat/frontend/src/components/Modal.vue
+++ djchat/frontend/src/components/Modal.vue
@@ -1,20 +1,23 @@
 <template>
-  <div
-    v-if="showing"
-    class="absolute inset-0 w-full h-screen flex items-center justify-center"
-    style="background-color: rgba(0, 0, 0, 0.75); z-index:1;"
-  >
-    <div class="relative w-full max-w-2xl bg-white shadow-lg rounded-lg p-8">
-      <button
-        aria-label="close"
-        class="absolute top-0 right-0 text-xl text-gray-500 my-2 mx-4"
-        @click.prevent="close"
-      >
-        ×
-      </button>
-      <slot />
+  <Transition name="fade">
+    <div
+      v-if="showing"
+      class="absolute inset-0 w-full h-screen flex items-center justify-center"
+      style="background-color: rgba(0, 0, 0, 0.75); z-index:1;"
+      @click.self="close"
+    >
+      <div class="relative w-full max-w-2xl bg-white shadow-lg rounded-lg p-8">
+        <button
+          aria-label="close"
+          class="absolute top-0 right-0 text-xl text-gray-500 my-2 mx-4"
+          @click.prevent="close"
+        >
+          ×
+        </button>
+        <slot />
+      </div>
     </div>
-  </div>
+  </Transition>
 </template>
 
 <script>
@@ -25,6 +28,14 @@ export default {
       type: Boolean
     }
   },
+  watch: {
+    showing(value) {
+      if (value) {
+        return document.querySelector("body").classList.add("overflow-hidden");
+      }
+      document.querySelector("body").classList.remove("overflow-hidden");
+    }
+  },
   methods: {
     close() {
       this.$emit("close");
@@ -32,3 +43,14 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.4s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
--- djchat/frontend/src/views/Home.vue
+++ djchat/frontend/src/views/Home.vue
@@ -1,9 +1,36 @@
 <template>
   <div class="home">
-    <div class="flex flex-wrap mt-5">
+    <div class="flex flex-wrap">
       <div class="w-1/3 p-3">
         <!-- Seccion conversaciones  -->
         <users-section />
+
+        <div
+          class="shadow-md border rounded-lg bg-white mt-3"
+          style="min-height: 20vh;"
+        >
+          <p>Modal</p>
+          <button
+            class="bg-blue-600 text-white px-4 py-2 text-sm uppercase tracking-wide font-bold rounded-lg"
+            @click="showModal = true"
+          > Show Modal
+          </button>
+
+          <card-modal
+            :showing="showModal"
+            @close="showModal = false"
+          >
+            <h2 class="text-xl font-bold text-gray-900">Example modal</h2>
+            <p class="mb-6">This is example text passed through to the modal via a slot.</p>
+            <button
+              class="bg-blue-600 text-white px-4 py-2 text-sm uppercase tracking-wide font-bold rounded-lg"
+              @click="showModal = false"
+            >
+              Close
+            </button>
+          </card-modal>
+
+        </div>
       </div>
 
       <div class="w-2/3 p-3">
@@ -17,9 +44,10 @@
 
             <!-- Footer Copyright -->
             <div class="text-right">
-              <a style="font-size: 9px;" href="http://www.freepik.com"
-                >Background vector created by kjpargeter - www.freepik.com</a
-              >
+              <a
+                style="font-size: 9px;"
+                href="http://www.freepik.com"
+              >Background vector created by kjpargeter - www.freepik.com</a>
             </div>
           </div>
         </div>
@@ -33,11 +61,19 @@ import SendForm from "@/components/SendForm.vue";
 import MessagesSection from "@/components/messages/MessagesSection.vue";
 import UsersSection from "@/components/users/UsersSection.vue";
 
+import CardModal from "@/components/Modal.vue";
+
 export default {
   components: {
     SendForm,
     MessagesSection,
-    UsersSection
+    UsersSection,
+    CardModal
+  },
+  data() {
+    return {
+      showModal: false
+    };
   }
 };
 </script>
