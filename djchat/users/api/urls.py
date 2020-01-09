daac54ad813fca5589e7ae476329efb5e34ba5d4
santiago <stgoddv@gmail.com>
2020-01-09 03:18:08 -0300

    added boilerplate for link and invitations feature

--- djchat/users/api/urls.py
+++ djchat/users/api/urls.py
@@ -3,5 +3,18 @@ from users.api.views import CurrentUserAPIView, UsersAPIView
 
 urlpatterns = [
     path('me/', CurrentUserAPIView.as_view(), name='current-user'),
-    path('users/', UsersAPIView.as_view())
+    path('users/', UsersAPIView.as_view()),
+
+    # path('invitations', InvitationAPIView.as_view()),
+    # path('invitations/<int:invitation_id>', InvitationAPIView.as_view()),
+    # path('invitations?direction=sent|received', InvitationAPIView.as_view()),
+    # path('invitations/create', CreateInvitationAPIView.as_view()),
+
+    # path('invitations/<int:invitation_id>/accept', UsersAPIView.as_view()),
+    # path('invitations/<int:invitation_id>/decline', UsersAPIView.as_view()),
+
+    # path('users/friends', UsersAPIView.as_view()),
+    # path('users/friends/<int:friend_id>', UsersAPIView.as_view()),
+    # path('users/friends/<int:friend_id>/delete', UsersAPIView.as_view()),
+    # path('users/friends/<int:friend_id>/block', UsersAPIView.as_view()),
 ]
--- djchat/users/api/views.py
+++ djchat/users/api/views.py
@@ -8,14 +8,29 @@ User = get_user_model()
 
 
 class UsersAPIView(APIView):
-
     def get(self, request):
         serializer = UserSerializer(User.objects.all(), many=True)
         return Response(serializer.data)
 
 
 class CurrentUserAPIView(APIView):
-
     def get(self, request):
         serializer = UserSerializer(request.user)
         return Response(serializer.data)
+
+
+class CreateInvitationAPIView(APIView):
+    def post(self, request, format=None):
+        # Validation by serialization
+        # Two different existing users are required
+        # Check if invitation already exists
+        # Check if users have already a relationship
+        # Create the invitation
+        # Send invitation signal
+        # Response OK
+        pass
+
+
+class InvitationAPIView(APIView):
+    def get(self, request, format=None):
+        pass
