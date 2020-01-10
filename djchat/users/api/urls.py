4f1b21705275ffee9d9922e418c6aa604211bd0e
santiago <stgoddv@gmail.com>
2020-01-10 04:17:05 -0300

    removed unused models in users app

--- djchat/users/api/urls.py
+++ djchat/users/api/urls.py
@@ -4,19 +4,4 @@ from users.api.views import CurrentUserAPIView, UsersAPIView
 urlpatterns = [
     path('me/', CurrentUserAPIView.as_view(), name='current-user'),
     path('users/', UsersAPIView.as_view()),
-
-    # path('invitations', InvitationAPIView.as_view()),
-    # path('invitations?direction=sent|received', InvitationAPIView.as_view()),
-    # path('invitations/<int:invitation_id>', InvitationAPIView.as_view()),
-
-    # path('invitations/create', CreateInvitationAPIView.as_view()),
-
-    # path('invitations/<int:invitation_id>/accept', AcceptInvitationAPIView.as_view()),
-    # path('invitations/<int:invitation_id>/decline', DeclineInvitationAPIView.as_view()),
-
-    # path('users/friends', FriendsViewSet.as_view()),
-    # path('users/friends/<int:friend_id>', FriendsViewSet.as_view()),
-
-    # path('users/friends/<int:friend_id>/delete', UsersAPIView.as_view()),
-    # path('users/friends/<int:friend_id>/block', UsersAPIView.as_view()),
 ]
